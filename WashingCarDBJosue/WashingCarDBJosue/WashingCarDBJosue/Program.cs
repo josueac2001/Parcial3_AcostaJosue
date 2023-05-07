3eb32a4c1febe9c6d8778df02e0655b26c19d88c
JOSU� ACOSTA LONDO�O <josueacosta261853@correo.itm.edu.co>
2023-05-06 19:39:20 -0500

    RazorEngine Implemented to the project

--- WashingCarDBJosue/WashingCarDBJosue/WashingCarDBJosue/Program.cs
+++ WashingCarDBJosue/WashingCarDBJosue/WashingCarDBJosue/Program.cs
@@ -11,7 +11,10 @@ builder.Services.AddDbContext<DatabaseContext>(o =>
     o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
